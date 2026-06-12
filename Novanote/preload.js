d994216c76b5c6ee502183c72e90d8287b4ce969
Blevy <leverianbeatus@gmail.com>
2026-06-12 07:55:33 +0300

    Add IPC methods for saving, loading, and deleting days

--- Novanote/preload.js
+++ Novanote/preload.js
@@ -2,4 +2,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   loadPage: (page) => ipcRenderer.send("load-page", page),
+  saveDay: (key, data) => ipcRenderer.invoke("save-day", key, data),
+  loadDay: (key) => ipcRenderer.invoke("load-day", key),
+  getAllDates: () => ipcRenderer.invoke("get-all-dates"),
+  deleteDay: (key) => ipcRenderer.invoke("delete-day", key),
 });
